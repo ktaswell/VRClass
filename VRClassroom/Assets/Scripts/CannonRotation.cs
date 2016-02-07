d8b48fe84242f8dfebd870675817fb9407804682
ktaswell <ktaswell@BrainHealthAlliance.org>
2016-02-07 13:51:03 -0800

    CannonRotationUpdates

--- VRClassroom/Assets/Scripts/CannonRotation.cs
+++ VRClassroom/Assets/Scripts/CannonRotation.cs
@@ -8,14 +8,13 @@ public class CannonRotation : MonoBehaviour
   public float smooth = 2.0F;
   public float tiltAngle = 30.0F;
   public Rigidbody projectile;
-  public Vector3 raycastDirection;
   public Transform hit;
-  public float x, y;
+  public int ballCount = 0;
 
   const float angToRad = Mathf.PI / 360;
   private RaycastHit trajectory;
   private Vector3 projectilePosition;
-  private Transform cannon;
+  private GameObject[] projectileFired;
   private string velocity, angle;
   private int velocityVal = 0, angleVal = 0;
   private Vector3 angVelocity;
@@ -23,12 +22,12 @@ public class CannonRotation : MonoBehaviour
 
   void Start()
   {
-    cannon = GetComponent<Transform>();
-
     velocity = "0";
     angle = "0";
     velocityIn.text = velocity;
     angleIn.text = angle;
+
+    projectileFired = new GameObject[6]; 
   }
 
   // Update is called once per frame
@@ -43,7 +42,7 @@ public class CannonRotation : MonoBehaviour
     if (Input.GetButtonDown("Left") && angleVal > 0)
       angleVal -= 5;
     
-    Quaternion target = Quaternion.Euler(0, 0,  -1 * angleVal);
+    Quaternion target = Quaternion.Euler(0, 180,  -1 * angleVal);
     transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
     projectilePosition = hit.position;
     // in order to calculate in 3 dimensions  where theta is angle from x to y and phi is angle from x to z
@@ -51,18 +50,23 @@ public class CannonRotation : MonoBehaviour
     // Vy = V*sin(theta)
     // Vz = V*sin(phi)
     //to create projectile inside of cannon use some form of y=mx+b where m = slope of tangent to theta
-    x = -velocityVal * Mathf.Cos((360 - transform.rotation.eulerAngles.z) * angToRad);
-    y = velocityVal * Mathf.Sin((360 - transform.rotation.eulerAngles.z) * angToRad);
-    angVelocity = new Vector3(x, y, 0F);
     
-    if (Input.GetButtonDown("Fire1"))
+    if (Input.GetButtonDown("Fire1") && ballCount < 6)
     {
-      Rigidbody projectileFired = Instantiate(projectile, projectilePosition, projectile.transform.rotation) as Rigidbody;
-      projectileFired.velocity = angVelocity; 
+      projectileFired[ballCount] = Instantiate(projectile, projectilePosition, projectile.transform.rotation) as GameObject;
+      Rigidbody rb = projectileFired[ballCount].GetComponent<Rigidbody>();
+      rb.velocity = transform.right * -velocityVal;
+      ballCount++;
+    }
+
+    if (Input.GetKeyDown(KeyCode.R) || ballCount == 5)
+    {
+      for (ballCount = 5; ballCount > 0; ballCount--)
+        Destroy(projectileFired[ballCount]);
     }
 
     velocity = velocityVal.ToString();
-    angle = angleVal.ToString();
+    angle = angleVal.ToString() + "";
     velocityIn.text = velocity;
     angleIn.text = angle;
   }
