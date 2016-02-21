bb9c9cf883b933bbe0e975e1949e3ad2ffed1aac
ktaswell <ktaswell@BrainHealthAlliance.org>
2016-02-21 11:24:58 -0800

    update

--- VRClassroom/Assets/Scripts/CannonRotation.cs
+++ VRClassroom/Assets/Scripts/CannonRotation.cs
@@ -4,30 +4,31 @@ using UnityEngine.UI;
 public class CannonRotation : MonoBehaviour
 {
   //public GameObject projectile;
-  public Text velocityIn, angleIn;
+  public Text velocityIn, angleIn, distOut;
   public float smooth = 2.0F;
   public float tiltAngle = 30.0F;
   public Rigidbody projectile;
   public Transform hit;
   public int ballCount = 0;
 
-  const float angToRad = Mathf.PI / 360;
-  private RaycastHit trajectory;
   private Vector3 projectilePosition;
-  private GameObject[] projectileFired;
-  private string velocity, angle;
+  private GameObject projFiredObj;
+  public Rigidbody projectileFired;
+  private string velocity, angle, dist;
+  private float distVal = 0;
   private int velocityVal = 0, angleVal = 0;
-  private Vector3 angVelocity;
+  private bool test;
   // Use this for initialization
 
   void Start()
   {
     velocity = "0";
     angle = "0";
+    dist = "N/A";
     velocityIn.text = velocity;
     angleIn.text = angle;
 
-    projectileFired = new GameObject[6]; 
+    projectileFired = projectile;
   }
 
   // Update is called once per frame
@@ -41,8 +42,8 @@ public class CannonRotation : MonoBehaviour
       angleVal += 5;
     if (Input.GetButtonDown("Left") && angleVal > 0)
       angleVal -= 5;
-    
-    Quaternion target = Quaternion.Euler(0, 180,  -1 * angleVal);
+
+    Quaternion target = Quaternion.Euler(0, 180, -1 * angleVal);
     transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
     projectilePosition = hit.position;
     // in order to calculate in 3 dimensions  where theta is angle from x to y and phi is angle from x to z
@@ -50,24 +51,38 @@ public class CannonRotation : MonoBehaviour
     // Vy = V*sin(theta)
     // Vz = V*sin(phi)
     //to create projectile inside of cannon use some form of y=mx+b where m = slope of tangent to theta
-    
-    if (Input.GetButtonDown("Fire1") && ballCount < 6)
+
+    if (Input.GetButtonDown("Fire1") && ballCount < 1)
     {
-      projectileFired[ballCount] = Instantiate(projectile, projectilePosition, projectile.transform.rotation) as GameObject;
-      Rigidbody rb = projectileFired[ballCount].GetComponent<Rigidbody>();
-      rb.velocity = transform.right * -velocityVal;
+      projectileFired = Instantiate(projectile, projectilePosition, projectile.transform.rotation) as Rigidbody;
+      projectileFired.velocity = transform.right * -velocityVal;
+      projFiredObj = projectileFired.gameObject;
       ballCount++;
+      test = true;
     }
 
-    if (Input.GetKeyDown(KeyCode.R) || ballCount == 5)
+    if (projectileFired && test)
     {
-      for (ballCount = 5; ballCount > 0; ballCount--)
-        Destroy(projectileFired[ballCount]);
-    }
+      if (projectileFired.velocity == Vector3.zero)
+      {
+        projFiredObj = projectileFired.gameObject;
+        distVal = Mathf.Abs(transform.position.x + projFiredObj.transform.position.x)/2;
+        dist = distVal.ToString();
+      }
 
+      if (Input.GetKeyDown(KeyCode.R))
+      {
+        projFiredObj = projectileFired.gameObject;
+        Destroy(projFiredObj);
+        dist = "N/A";
+        ballCount = 0;
+        test = false;
+      }
+    }
     velocity = velocityVal.ToString();
     angle = angleVal.ToString() + "";
     velocityIn.text = velocity;
     angleIn.text = angle;
+    distOut.text = dist;
   }
 }
---
+++ VRClassroom/Assets/Scripts/DistStop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistStop : MonoBehaviour
+{
+
+  private Rigidbody rb;
+
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.CompareTag("Floor"))
+    {
+      rb.velocity = Vector3.zero;
+    }
+  }
+
+}
